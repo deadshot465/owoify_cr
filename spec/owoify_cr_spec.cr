5af41b1378790b6725ddfb326c1e9cd656889452
Gary Chou <deadshot465@gmail.com>
2021-05-16 10:48:50 +0900

    Add more test cases.

--- spec/owoify_cr_spec.cr
+++ spec/owoify_cr_spec.cr
@@ -1,6 +1,8 @@
 require "./spec_helper"
 
-SOURCE = "Hello, World! This is the string to owo! Kinda cute, isn't it?"
+SOURCE                 = "Hello, World! This is the string to owo! Kinda cute, isn't it?"
+POKEMON_NAME_LIST_PATH = "assets/pokemons.txt"
+WAR_AND_PEACE_PATH     = "assets/war_and_peace_chapter01-20.txt"
 
 describe Owoify do
   it "should owoify the source string" do
@@ -11,16 +13,19 @@ describe Owoify do
   it "should owoify the source string with level of owo" do
     result = Owoify.owoify(SOURCE)
     result.should_not eq("")
+    result.should_not eq(nil)
   end
 
   it "should owoify the source string with level of uwu" do
     result = Owoify.owoify(SOURCE, "uwu")
     result.should_not eq("")
+    result.should_not eq(nil)
   end
 
   it "should owoify the source string with level of uvu" do
     result = Owoify.owoify(SOURCE, "uvu")
     result.should_not eq("")
+    result.should_not eq(nil)
   end
 
   it "should give different result when levels are owo and uwu" do
@@ -37,5 +42,32 @@ describe Owoify do
 
   expect_raises(Exception, "not supported") do
     Owoify.owoify(SOURCE, "123")
+  end
+
+  it "should owoify pokemon names" do
+    File.read_lines(POKEMON_NAME_LIST_PATH).each do |name|
+      name_with_owo = Owoify.owoify(name)
+      name_with_uwu = Owoify.owoify(name, "uwu")
+      name_with_uvu = Owoify.owoify(name, "uvu")
+      name_with_owo.should_not eq("")
+      name_with_uwu.should_not eq("")
+      name_with_uvu.should_not eq("")
+      name_with_owo.should_not eq(nil)
+      name_with_uwu.should_not eq(nil)
+      name_with_uvu.should_not eq(nil)
+    end
+  end
+
+  it "should owoify long text" do
+    text = File.read(WAR_AND_PEACE_PATH)
+    text_with_owo = Owoify.owoify(text)
+    text_with_uwu = Owoify.owoify(text, "uwu")
+    text_with_uvu = Owoify.owoify(text, "uvu")
+    text_with_owo.should_not eq("")
+    text_with_uwu.should_not eq("")
+    text_with_uvu.should_not eq("")
+    text_with_owo.should_not eq(nil)
+    text_with_uwu.should_not eq(nil)
+    text_with_uvu.should_not eq(nil)
   end
 end
